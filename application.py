0ef7b32fdff5e0959c0c1ce96c74497f0527c352
Aravind Adithya <aravindadithya95@gmail.com>
2017-11-24 10:18:44 -0500

    Add routes

--- application.py
+++ application.py
@@ -2,6 +2,33 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+@app.route('/catalog')
+def show_catalog():
+    return "List of categories."
+
+
+@app.route('/catalog/<string:category_name>/')
+@app.route('/catalog/<string:category_name>/items/')
+def show_category(category_name):
+    return "List of items in that category."
+
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/')
+def show_item(category_name, item_name):
+    return "The selected item."
+
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/edit/')
+def edit_item(category_name, item_name):
+    return "The page to edit the selected item."
+
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/delete/')
+def delete_item(category_name, item_name):
+    return "The page to delete the selected item."
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
