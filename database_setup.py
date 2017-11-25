a0886e989ab108fb0d46016ccbbbb55ed4f86469
Aravind Adithya <aravindadithya95@gmail.com>
2017-11-24 22:15:11 -0500

    Update database setup

--- database_setup.py
+++ database_setup.py
@@ -20,9 +20,6 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-
-    user = relationship(User)
 
 
 class Item(Base):
