b67bf0a30d257643f72f36bf5a154bbff9979d88
Aravind Adithya <aravindadithya95@gmail.com>
2017-12-03 19:16:07 -0500

    Bug fix

--- populate_db.py
+++ populate_db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Base, User, Category, Item
+from models import Base, User, Category, Item
 
 # Connect to the database
 engine = create_engine('postgresql:///catalog')
